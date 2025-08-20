c208f397585c5c49b41460f5b7203888428b6ddc
Diogo de Oliveira <diogop20001@gmail.com>
2025-08-20 18:46:35 -0300

    Primeiro commit

---
+++ Lista.java
@@ -0,0 +1,10 @@
+import java.sql.Date;
+import java.time.*;
+class Lista {
+    private String nome;
+    private String descricao;
+    private Date criacao;
+    private Date limite;
+    private String codigo;
+    
+}
